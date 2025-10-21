3485add9d3bafa58043b26c8088c12b1bbcee14d
Mir Shifat Mahmud <mir19438.bdcallingit@gmail.com>
2025-10-21 17:52:44 +0600

    up

--- app/Http/Requests/Admin/AddSubscriptionRequest.php
+++ app/Http/Requests/Admin/AddSubscriptionRequest.php
@@ -22,33 +22,30 @@ class AddSubscriptionRequest extends FormRequest
      */
     public function rules(): array
     {
+        return [
+            'plan_name' => [
+                'required',
+                'string',
+                'max:255',
+                function ($attribute, $value, $fail) {
+                    if ($value == 'Free') {
+                        $fail('You cannot create a plan with the name Free! Please enter a different name.');
+                    }
 
-       return [
-    'plan_name' => [
-        'required',
-        'string',
-        'max:255',
-        function ($attribute, $value, $fail) {
-            // চেক করুন যে প্ল্যানের নাম 'Free' হলে
-            if ($value == 'Free') {
-                $fail('You cannot create a plan with the name Free! Please enter a different name.');
-            }
+                    $existingPlan = DB::table('subscriptions')
+                        ->where('plan_name', $value)
+                        ->first();
 
-            // এখন চেক করা হচ্ছে যে 'Free' ছাড়া অন্য কোন প্ল্যান আগে থেকেই আছে কিনা
-            $existingPlan = DB::table('subscriptions')
-                ->where('plan_name', $value)
-                ->first();
-
-            if ($existingPlan) {
-                $fail('A plan with this name already exists. Please enter a different name.');
-            }
-        },
-    ],
-    'duration' => 'required|string|in:monthly,yearly',
-    'price' => 'required|numeric|min:0',
-    'features' => 'required|array|min:1',
-    'features.*' => 'string|max:255',
-];
+                    if ($existingPlan) {
+                        $fail('A plan with this name already exists. Please enter a different name.');
+                    }
+                },
+            ],
+            'duration' => 'required|string|in:monthly,yearly',
+            'price' => 'required|numeric|min:0',
+            'features' => 'required|array|min:1',
+            'features.*' => 'string|max:255',
+        ];
 
     }
 }
