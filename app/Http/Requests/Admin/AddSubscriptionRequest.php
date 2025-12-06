072ab219647a126c9b8fecf29559f79f647f806d
Mir Shifat Mahmud <mir19438.bdcallingit@gmail.com>
2025-12-06 14:49:34 +0600

    fix

--- app/Http/Requests/Admin/AddSubscriptionRequest.php
+++ app/Http/Requests/Admin/AddSubscriptionRequest.php
@@ -43,7 +43,7 @@ class AddSubscriptionRequest extends FormRequest
             ],
             'duration' => 'required|string|in:monthly,yearly',
             'price' => 'required|numeric|min:1',
-            'discount' => 'required|numeric|min:0',
+            'discount' => 'nullable|numeric|min:0',
             'features' => 'required|array',
             'features.*' => 'string|max:255',
         ];
